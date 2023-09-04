4a8f9b9c6066a0085077517a4edc412a80b2bb0a
Gihun Ham <ham@e-mirim.hs.kr>
2023-09-04 13:40:31 +0900

    제네릭 구현 예제

--- GenericPrac31/Program.cs
+++ GenericPrac31/Program.cs
@@ -6,10 +6,25 @@ using System.Threading.Tasks;
 
 namespace GenericPrac31
 {
+    class Wanted<T>
+    {
+        public T Value;
+        public Wanted (T value)
+        {
+            Value = value;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            Wanted<string> wantedString = new Wanted<string>("String");
+            Wanted<int> wantedInt = new Wanted<int>(52273);
+            Wanted<double> wantedDouble = new Wanted<double>(52.273);
+
+            Console.WriteLine(wantedString.Value);
+            Console.WriteLine(wantedInt.Value);
+            Console.WriteLine(wantedDouble.Value);
         }
     }
 }
